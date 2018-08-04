7543302add07f5654de0c2bf53364739db0cdab4
MacBookPro <MacBookPro@rub159.mi03.mi.interbusiness.it>
2018-08-04 18:12:07 +0300

    change the link between reg_js and reg_html

--- js/register.js
+++ js/register.js
@@ -2,3 +2,33 @@ function testing()
 {
     alert("Hi Yousf");
 }
+
+
+$(function() {
+    $('#register-form-link').click();
+});
+
+$(function() {
+    $('#t_register').click();
+});
+
+
+
+//$(function() {
+//
+//    $('#login-form-link').click(function(e) {
+//		$("#sectionA").delay(100).fadeIn(100);
+// 		$("#sectionB").fadeOut(100);
+//		$('#register-form-link').removeClass('active');
+//		$(this).addClass('active');
+//		e.preventDefault();
+//	});
+//	$('#register-form-link').click(function(e) {
+//		$("#sectionB").delay(100).fadeIn(100);
+// 		$("#sectionA").fadeOut(100);
+//		$('#login-form-link').removeClass('active');
+//		$(this).addClass('active');
+//		e.preventDefault();
+//	});
+//
+//});
